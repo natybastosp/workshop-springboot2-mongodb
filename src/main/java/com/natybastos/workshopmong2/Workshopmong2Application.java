8ea1ed3af9a29407a51e23d1b424e1caedbf96c7
natybastosp <natybastos401@gmail.com>
2021-08-06 18:08:20 -0300

    first commit

---
+++ src/main/java/com/natybastos/workshopmong2/Workshopmong2Application.java
@@ -0,0 +1,13 @@
+package com.natybastos.workshopmong2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmong2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmong2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/natybastos/workshopmong2/Workshopmong2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natybastos.workshopmong2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmong2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
