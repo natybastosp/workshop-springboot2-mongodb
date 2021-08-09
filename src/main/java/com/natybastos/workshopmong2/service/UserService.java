ddde7e5354daca872378b1cd95fa5a7554d21401
natybastosp <natybastos401@gmail.com>
2021-08-09 10:33:05 -0300

    Atualização de usuário com PUT

--- src/main/java/com/natybastos/workshopmong2/service/UserService.java
+++ src/main/java/com/natybastos/workshopmong2/service/UserService.java
@@ -16,25 +16,36 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundExeption("Objeto não encontrado"));
-		}
+	}
 
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateDate(newObj, obj);
+		return repo.save(newObj);
+	}
 	
+	public void updateDate(User newobj, User obj) {
+		newobj.setName(obj.getName());
+		newobj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
