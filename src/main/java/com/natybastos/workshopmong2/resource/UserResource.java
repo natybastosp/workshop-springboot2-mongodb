348ecd13a4511d795d647ec808a13fa58274f87c
natybastosp <natybastos401@gmail.com>
2021-08-09 10:07:15 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/natybastos/workshopmong2/resource/UserResource.java
+++ src/main/java/com/natybastos/workshopmong2/resource/UserResource.java
@@ -1,11 +1,15 @@
 package com.natybastos.workshopmong2.resource;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.natybastos.workshopmong2.domain.User;
 import com.natybastos.workshopmong2.dto.UserDTO;
@@ -22,5 +26,19 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@ PathVariable String Id){
 		User obj = service.findById(Id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@ PathVariable String Id){
+		service.delete(Id);
+		return ResponseEntity.noContent().build();
 	}
 }
