5baff3e61d3c71874a1764cc9816a2562d2fb385
natybastosp <natybastos401@gmail.com>
2021-08-09 11:28:07 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/natybastos/workshopmong2/config/Instantiation.java
+++ src/main/java/com/natybastos/workshopmong2/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.natybastos.workshopmong2.domain.Post;
 import com.natybastos.workshopmong2.domain.User;
+import com.natybastos.workshopmong2.dto.AuthorDTO;
 import com.natybastos.workshopmong2.repository.PostRepository;
 import com.natybastos.workshopmong2.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria) );
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 
