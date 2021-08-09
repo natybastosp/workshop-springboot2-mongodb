784c0f7d6f2bb3c87a746334ee5214b9e16739f7
natybastosp <natybastos401@gmail.com>
2021-08-09 11:39:30 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/natybastos/workshopmong2/domain/User.java
+++ src/main/java/com/natybastos/workshopmong2/domain/User.java
@@ -1,9 +1,12 @@
 package com.natybastos.workshopmong2.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collation = "user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 		
 	public User() {
 	}
@@ -52,6 +58,14 @@ public class User implements Serializable {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
