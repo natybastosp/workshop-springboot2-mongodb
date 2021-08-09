63a412e05447d2a09d6f35bb35d2d79434a81d29
natybastosp <natybastos401@gmail.com>
2021-08-09 14:39:41 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/natybastos/workshopmong2/domain/Post.java
+++ src/main/java/com/natybastos/workshopmong2/domain/Post.java
@@ -1,12 +1,15 @@
 package com.natybastos.workshopmong2.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.natybastos.workshopmong2.dto.AuthorDTO;
+import com.natybastos.workshopmong2.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -78,7 +91,6 @@ public class Post implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
